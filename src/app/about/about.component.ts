3a2a624b3c1ab6005b319d2bb9b400fc35210774
Dejan Dudukovic <dexterns88@gmail.com>
2017-07-04 17:31:29 +0200

    Update animation on about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, HostBinding, setTestabilityGetter} from '@angular/core';
+
+import {query, stagger, animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations: [
+    trigger('pageAnimation', [
+
+      transition(':enter', [
+        query('.anim-segment', style({
+          opacity: 0
+        })),
+
+        query('.anim-segment', [
+          stagger(250, [
+            animate('800ms cubic-bezier(.35,0,.25,1)', style('*'))
+          ])
+        ])
+      ]),
+      transition(':leave', [
+        query('.anim-segment', [
+          stagger(150, [
+            animate('550ms cubic-bezier(.35,0,.25,1)', style({
+              opacity: 0
+            }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
+
+  @HostBinding('@pageAnimation')
+  public animatePage = true;
 
   constructor() { }
 
