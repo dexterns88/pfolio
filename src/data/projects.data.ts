cc196a81eaaf1e2fd5fc45842d4dab717b89b798
Dejan Dudukovic <dexterns88@gmail.com>
2017-07-17 09:21:49 +0200

    add data to gitignore

---
+++ src/data/projects.data.ts
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: 'Heawen water',
+    titleDesc: 'Company website for presentation',
+    content: 'heaven.md',
+    teach: [
+      'multi-language', 'php', 'custom-cms', 'html', 'css', 'responsive', 'jQuery'
+    ],
+    link: 'http://www.heaven-water.com',
+    images: [
+      'heaven/heaven1.png',
+      'heaven/heaven2.png',
+      'heaven/heaven3.png',
+      'heaven/heaven4.png',
+      'heaven/heaven5.png'
+    ]
+  },
+  {
+    title: 'Vila izvor',
+    titleDesc: 'Web presentation',
+    content: 'vilaizvor.md',
+    teach: [
+      'php', 'html', 'css', 'jQuery'
+    ],
+    link: 'http://www.vilaizvor.com',
+    images: [
+      'vilaizvor/izvor1.png',
+      'vilaizvor/izvor2.png',
+      'vilaizvor/izvor3.png',
+      'vilaizvor/izvor4.png',
+      'vilaizvor/izvor5.png'
+    ]
+  },
+  {
+    title: 'Pejin',
+    titleDesc: 'Small company website',
+    content: 'pejin.md',
+    teach: [
+      'html', 'css', 'jQuery'
+    ],
+    images: [
+      'pejin/pejin1.png',
+      'pejin/pejin2.png',
+      'pejin/pejin3.png'
+    ]
+  }
+];
